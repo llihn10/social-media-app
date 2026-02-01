93d45938eecbc3fd3a71f3d1be55e77ff5dd4e29
llihn10 <nblinggg10@gmail.com>
2026-02-01 11:45:02 +0700

    feat(homescreen): add header and tabs

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,7 +1,8 @@
 import PostCard from '@/components/PostCard';
 import { Link } from 'expo-router';
 import { useEffect, useState } from 'react';
-import { Text, View, FlatList } from 'react-native';
+import { Text, View, FlatList, ScrollView, TouchableOpacity } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
@@ -11,6 +12,7 @@ export default function HomeScreen() {
   const [posts, setPosts] = useState<any[]>([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState<string | null>(null);
+  const [activeTab, setActiveTab] = useState('foryou');
 
   useEffect(() => {
     const connectBackend = async () => {
@@ -56,32 +58,65 @@ export default function HomeScreen() {
     fetchPosts();
   }, [])
 
+  const Header = () => (
+    <View className='items-center pt-4 border-b border-gray-100 bg-secondary'>
+      <Text className='text-2xl font-semibold text-dark-100 mb-4'>The Hut</Text>
+
+      <View className='flex-row w-full justify-around'>
+        {/* Tab - For you */}
+        <TouchableOpacity
+          className='flex-1 items-center pb-2'
+          onPress={() => setActiveTab('foryou')}
+        >
+          <Text className={`font-bold ${activeTab === 'foryou' ? 'text-dark-100' : 'text-dark-200'}`}>
+            For you
+          </Text>
+          {activeTab === 'foryou' && (
+            <View className='absolute bottom-0 h-[3px] w-16 bg-primary rounded-full' />
+          )}
+        </TouchableOpacity>
+
+        {/* Tab - Following */}
+        <TouchableOpacity
+          className='flex-1 items-center pb-2'
+          onPress={() => setActiveTab('following')}
+        >
+          <Text className={`font-bold ${activeTab === 'following' ? 'text-dark-100' : 'text-dark-200'}`}>
+            Following
+          </Text>
+          {activeTab === 'following' && (
+            <View className='absolute bottom-0 h-[3px] w-16 bg-primary rounded-full' />
+          )}
+        </TouchableOpacity>
+      </View>
+    </View>
+  )
+
   if (loading) return null;
   if (error) return null;
 
   return (
-    <View className="">
-      <Text className="text-5xl text-accent font-bold">Welcome!</Text>
+    <SafeAreaView className='flex-1 bg-secondary'>
 
-      <Text className="text-base text-gray-600">
-        {message}
-      </Text>
+      <FlatList
+        data={posts}
+        // data={activeTab === 'foryou' ? postsForYou : postsFollowing}
+        keyExtractor={(item) => item._id}
+        renderItem={({ item }) => (<PostCard post={item} />)}
+
+        ListHeaderComponent={Header}
+
+        extraData={activeTab}
+
+        ItemSeparatorComponent={() => (<View className="border-t border-gray-200" />)}
+        showsVerticalScrollIndicator={false}
+      />
 
       <Link href="/profile" className="text-blue-500">
         Profile
       </Link>
 
-      <FlatList
-        data={posts}
-        keyExtractor={(item) => item._id}
-        renderItem={({ item }) => (
-          <PostCard post={item} />
-        )}
-        ItemSeparatorComponent={() => (
-          <View className="border-t border-gray-200" />
-        )}
-      />
-    </View>
+    </SafeAreaView>
   );
 }
 
